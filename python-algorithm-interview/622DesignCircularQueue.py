cd7da018eb1b6ca83071e8931d537a625c8e3ad2
2SEHI <58774664+2SEHI@users.noreply.github.com>
2021-09-11 20:57:32 +0900

    Modify CircularQueue

--- python-algorithm-interview/622DesignCircularQueue.py
+++ python-algorithm-interview/622DesignCircularQueue.py
@@ -2,117 +2,70 @@ class MyCircularQueue:
     def __init__(self, maxlen):
         self.q = [None] * maxlen
         self.maxlen = maxlen
-        self.f = 0 # 맨 앞 인덱스
-        self.r = 0 # 맨 끝 인덱스
+        self.f = -1 # 맨 앞 인덱스
+        self.r = -1 # 맨 끝 인덱스
 
-    def enQueue(self, input):
-        i = self.r
-        # 가득 차있으면 False반환
-        if self.isFull() :
-            return False
-        # 채울 공간이 있으면 끝 인덱스 + 1
-        else :
+    # 저장 가능 공간(None의 개수)이 0개이면 True
+    # 저장 가능 공간(None의 개수)이 0개보다 크면 False
+    def isFull(self):
+        return self.q.count(None) == 0
 
-            if self.r >= len(self.q)-1 or self.isEmpty():
-                self.r = 0
-            else :
-                self.r += 1
+    # 저장 가능 공간(None의 개수)이 큐의 크기와 같으면 True
+    # 저장 가능 공간(None의 개수)이 큐의 크기보다 작으면 False
+    def isEmpty(self):
+        return self.q.count(None) == self.maxlen
 
-        self.q[self.r] = input
-        print("삽입 후 queue:", self.q)
-        return True
-
-    def deQueue(self):
-        # 삭제할 데이터가 존재하지 않으면 False반환
+    # 큐의 맨앞 데이터를 반환
+    # isEmpty가 True일 경우 -1 반환
+    def Front(self):
         if self.isEmpty():
-            return False
-        else :
-            # 삭제할 데이터가 존재하면 삭제
-            self.q[self.f] = None
-            # 삭제 후 데이터가 없으면 self.f와 self.r을 0설정
-            if self.isEmpty():
-                self.f, self.r = 0, 0
-            else :
-                if self.r >= len(self.q) - 1 or self.isEmpty():
-                    self.r = 0
-                else:
-                    self.r += 1
-            print("삭제 후 queue:", self.q)
-            return True
+            return -1
+        else:
+            return self.q[self.f]
 
+    # 큐의 맨뒤 데이터를 반환
+    # isEmpty가 True일 경우 -1 반환
     def Rear(self):
         if self.isEmpty():
             return -1
-        else :
+        else:
             return self.q[self.r]
 
-    def Front(self):
-        print(self.q)
+    def enQueue(self, input):
+        r = self.r + 1
+        # isFull이 True라면 False반환
+        if self.isFull():
+            return False
+
+        # isEmpty가 True라면 f를 1 증가
         if self.isEmpty():
-            return -1
+            self.f = self.f + 1
+        # r이 maxlen보다 작으면 r+1 지점에 input을 저장
+        if r < self.maxlen:
+            self.r = self.r + 1
         else :
-            return self.q[self.f]
+            # r이 maxlen과 같으면 0 지점에 input을 저장
+            self.r = 0
+        self.q[self.r] = input
+        return True
 
-    # 데이터가 1도 없으면 True반환
-    def isEmpty(self):
-        return self.q.count(None) == len(self.q)
+    def deQueue(self):
+        f = self.f + 1
+        print('삭제하기 전 : ', self.q)
+        # isEmpty이 True이면 False반환
+        if self.isEmpty():
+            return False
 
-    # 데이터가 가득 찼으면 True, 공간이 남았으면 False
-    def isFull(self):
+        self.q[self.f] = None
+        # 데이터 삭제후 isEmpty가 True이면 f와 r에 -1저장
+        if self.isEmpty():
+            self.f = -1
+            self.r = -1
+        else :
+            if f < self.maxlen:
+                self.f = self.f + 1
+            else :
+                self.f = 0
+        print('삭제후 : ', self.q)
+        return True
 
-        return self.q.count(None) == 0
-
-
-
-if __name__ == "__main__" :
-
-    # myCircularQueue = MyCircularQueue(3)
-    # print(myCircularQueue.enQueue(1))
-    # print(myCircularQueue.enQueue(2))
-    # print(myCircularQueue.enQueue(3))
-    # print(myCircularQueue.enQueue(4))
-    # print(myCircularQueue.Rear())
-    # print(myCircularQueue.isFull())
-    # print(myCircularQueue.deQueue())
-    # print(myCircularQueue.enQueue(4))
-    # print(myCircularQueue.Rear())
-    #
-    # circularQueue = MyCircularQueue(4)
-    # print(circularQueue.enQueue(10))
-    # print(circularQueue.enQueue(20))
-    # print(circularQueue.enQueue(30))
-    # print(circularQueue.enQueue(40))
-    # print(circularQueue.enQueue(50))
-    # print(circularQueue.Rear())
-    # print(circularQueue.isFull())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.enQueue(50))
-    # print(circularQueue.enQueue(60))
-    # print(circularQueue.Rear())
-    # print(circularQueue.Front())
-    #
-    # circularQueue = MyCircularQueue(6)
-    # print(circularQueue.enQueue(6))
-    # print(circularQueue.Rear())
-    # print(circularQueue.Rear())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.enQueue(5))
-    # print(circularQueue.Rear())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.Front())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.deQueue())
-    # print(circularQueue.deQueue())
-
-    circularQueue = MyCircularQueue(2)
-    print(circularQueue.enQueue(1))
-    print(circularQueue.enQueue(2))
-    print(circularQueue.deQueue())
-    print(circularQueue.enQueue(3))
-    print(circularQueue.deQueue())
-    print(circularQueue.enQueue(3))
-    print(circularQueue.deQueue())
-    print(circularQueue.enQueue(3))
-    print(circularQueue.deQueue())
-    print(circularQueue.Front())
